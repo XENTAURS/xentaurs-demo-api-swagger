a5d67ba21a21bd3f3b451bbe7627599011c3c92f
Brian Carpio <bcarpio@thetek.net>
2017-04-07 20:56:13 -0600

    updated for Cherry

--- api/settings.py
+++ api/settings.py
@@ -1,9 +1,10 @@
+import os
 from people import *
 # Let's just use the local mongod instance. Edit as needed.
 
 # Please note that MONGO_HOST and MONGO_PORT could very well be left
 # out as they already default to a bare bones local 'mongod' instance.
-MONGO_HOST = 'localhost'
+MONGO_HOST = os.getenv('MONGO_HOST', 'localhost')
 MONGO_PORT = 27017
 MONGO_DBNAME = 'eveapitest'
 
@@ -16,5 +17,6 @@ JSON_SORT_KEYS = True
 
 X_DOMAINS = ['http://localhost',  # The domain where Swagger UI is running
              'http://editor.swagger.io',
-             'http://petstore.swagger.io']
+             'http://petstore.swagger.io',
+             'http://swagger.demo.xentaurs.com']
 X_HEADERS = ['Content-Type', 'If-Match']  # Needed for the "Try it out" buttons
