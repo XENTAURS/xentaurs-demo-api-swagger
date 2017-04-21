455bf759c3e171b70311c34318d75473ffee78c8
Brian Carpio <bcarpio@thetek.net>
2017-04-21 03:29:42 -0600

    added Makefile

---
+++ api/domain.py
@@ -0,0 +1,13 @@
+from application import *
+from publish import *
+from promote import *
+from scale import *
+from login import *
+
+DOMAIN = {
+        'login': login,
+        'application': application,
+        'publish': publish,
+        'promote': promote,
+        'scale': scale,
+}
---
+++ api/publish.py
@@ -0,0 +1,56 @@
+schema = {
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/pyeve/cerberus) for details.
+    'firstname': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 10,
+    },
+    'lastname': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 15,
+        'required': True,
+    },
+    'email': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 50,
+        'required': True,
+        'unique': True,
+    },
+    # An embedded 'strongly-typed' dictionary.
+    'location': {
+        'type': 'dict',
+        'schema': {
+            'address': {'type': 'string'},
+            'city': {'type': 'string'},
+            'state': {'type': 'string'},
+            'zipcode': {'type': 'string'}
+        },
+        'required': True,
+    }
+}
+publish = {
+    # 'title' tag used in item links. Defaults to the resource title minus
+    # the final, plural 's' (works fine in most cases but not for 'people')
+    'item_title': 'publish',
+    'description': 'Publish Route To Make Services Public',
+
+    # by default the standard item entry point is defined as
+    # '/people/<ObjectId>'. We leave it untouched, and we also enable an
+    # additional read-only entry point. This way consumers can also perform
+    # GET requests at '/people/<lastname>'.
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'email'
+    },
+
+    # We choose to override global cache-control directives for this resource.
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+
+    # most global settings can be overridden at resource level
+    'resource_methods': ['GET', 'POST'],
+    'schema': schema
+}
